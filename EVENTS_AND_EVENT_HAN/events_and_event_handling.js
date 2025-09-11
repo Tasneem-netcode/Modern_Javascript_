b6a769840a43dd6807d4d15124d01bc03857715d
Tasneem-netcode <tasneem78899@gmail.com>
2025-09-12 01:40:23 +0530

    Events

--- EVENTS_AND_EVENT_HAN/events_and_event_handling.js
+++ EVENTS_AND_EVENT_HAN/events_and_event_handling.js
@@ -64,4 +64,124 @@ btn.addEventListener("click", () => {
 let p = document.querySelector("p");
 p.addEventListener("dblclick" , () => {
     p.style.color = "lightblue";
-})
+});
+
+function clickMe() {
+    alert("Button clicked!");
+}
+
+let bttn = document.querySelector(".BuyNow");
+bttn.style.backgroundColor = "pink";
+bttn.style.color = "Black";
+bttn.style.padding = "10px 20px";
+bttn.style.border = "none";
+bttn.style.borderRadius = "5px";
+bttn.style.cursor = "pointer";
+bttn.style.fontSize = "16px";
+bttn.style.margin = "10px";
+bttn.style.display = "block";
+bttn.style.margin = "auto";
+
+bttn.addEventListener("mouseenter", () => {
+    bttn.textContent = "Happy Shopping!";
+    bttn.style.backgroundColor = "lightgreen";
+});
+
+bttn.addEventListener("mouseleave", (e) => {  
+    bttn.textContent = "Buy Now";
+    bttn.style.backgroundColor = "pink";
+});
+
+// bttn.removeEventListener("mouseleave", () => {
+//     bttn.textContent = "Buy Now";
+//     bttn.style.backgroundColor = "pink";
+// });
+
+//common js events:
+//click:When an element is clicked 
+//dblclick:When an element is double clicked
+//mouseover:When the mouse pointer is moved onto an element
+let para= document.querySelector("#paragraph");
+para.addEventListener("mouseover", () => {
+    para.style.backgroundColor = "lightgrey";
+});
+
+para.addEventListener("mouseout", () => {
+    para.style.backgroundColor = "white";
+});
+//mouseout:When the mouse pointer is moved out of an element
+//mousedown:When a mouse button is pressed down on an element
+para.addEventListener("mousedown", () => {
+    para.style.color = "red";
+});
+
+//keyboard events:
+//keydown:When a keyboard key is pressed down
+// document.addEventListener("keydown", (e) => {
+//     console.log("key pressed on keyboard", e.key);
+// });
+
+// //keyup:When a keyboard key is released
+// document.addEventListener("keydown", (e) => {
+//     console.log("key released on keyboard", e.key);
+// });
+
+
+let ipt = document.querySelector("input");
+ipt.addEventListener("input", (e) => {
+    // console.log("Typed character:", e.data);
+    console.log("Current input value:", e.target.value);
+    if(e.data !== null) { //here e is an object and data is a object field which tells which key is pressed
+        console.log("Typed character:", e.data);
+    }
+});  
+
+//change:When the value of an element is changed and the element loses focus
+let select = document.querySelector("select");
+let dev = document.querySelector("#Devices");
+
+
+select.addEventListener("change", (e) => {
+    console.log(e);
+    console.log(e.target.value);
+    dev.textContent = `You selected ${e.target.value}`;
+});
+
+//eventlistener on the title heading 
+//we want any key that is pressed to be reflected in the screen heading
+// use window object to add event listener
+
+let h1 = document.querySelector("h1");
+
+window.addEventListener("keydown",(e) => {
+    console.log(e.key);
+    h1.textContent = e.key;
+    if(e.key === "shift" || e.key === "CapsLock"){
+        //dont show the heading text to shift or capslock
+        // h1.textContent =" ";
+        e.key = " ";
+    }
+});
+
+let btnforupload = document.querySelector("#btn_to_upload");
+let input = document.querySelector("#file_input");
+
+btnforupload.addEventListener("click", () =>{
+    input.click();
+});
+
+input.addEventListener("change", () => {
+    console.log(input.files[0]); // logs the first selected file
+    alert(`You selected: ${input.files[0].name}`);
+});
+
+input.addEventListener("change", (e) =>{
+    console.log(e.target.files);
+    console.log("you selected " , e.target.files[0].name);
+    const file = e.target.files[0];
+    if(file){
+        btnforupload.textContent = file.name;
+
+    }
+});
+
