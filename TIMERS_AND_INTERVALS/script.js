ede2efd04500ae4c35fb2f40a33a99bbff1bb508
Tasneem <tasneem78899@gmail.com>
2025-09-19 00:08:59 +0530

    time_and_intervals

---
+++ TIMERS_AND_INTERVALS/script.js
@@ -0,0 +1,8 @@
+//Timers:
+setTimeout(function() {
+    console.log("This message is shown after 3 seconds by setTimeout");
+}, 3000); //3000 milliseconds = 3 seconds
+
+setInterval(function() {
+    console.log("This message is shown every 2 seconds by setInterval");
+}, 2000); //2000 milliseconds = 2 seconds
