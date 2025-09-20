d37aa1d558ee2e4af630f360e6c667edeb03b291
Tasneem <tasneem78899@gmail.com>
2025-09-21 01:23:34 +0530

    theme_project

--- LOCAL_STORAGE_SESSION_STORAGE_AND_COOKIES/theme.js
+++ LOCAL_STORAGE_SESSION_STORAGE_AND_COOKIES/theme.js
@@ -1,3 +1,39 @@
- if(window.matchMedia("(prefers-color-scheme: dark)").matches){
+function setDarkOrLight(){
+    if(window.matchMedia("(prefers-color-scheme: dark)").matches){
     document.body.classList.add("dark");
+    document.body.classList.remove("light");
+}
+else{
+     document.body.classList.add("light");
+    document.body.classList.remove("dark");
  }
+}
+setDarkOrLight();
+
+document.body.classList.add(localStorage.getItem("theme") || "light");
+
+document.querySelector("#theme-toggle").addEventListener("click",()=>{
+    if(document.body.classList.contains("dark")){
+        document.body.classList.remove("dark");
+        document.body.classList.add("light");
+        // Save user preference in local storage
+        localStorage.setItem("theme", "light");
+        
+    }else{
+        document.body.classList.remove("light");
+        document.body.classList.add("dark");
+        // Save user preference in local storage
+        localStorage.setItem("theme", "dark");
+    }
+});
+// ...existing code...
+window.matchMedia("(prefers-color-scheme: dark)").addEventListener("change", (event) => {
+    if (event.matches) {
+        document.body.classList.add("dark");
+        document.body.classList.remove("light");
+    } else {
+        document.body.classList.add("light");
+        document.body.classList.remove("dark");
+    }
+});
+// ...existing code...
